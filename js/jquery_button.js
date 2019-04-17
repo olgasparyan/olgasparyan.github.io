30bc5db4725ac0ac2f06580cfd0f8c8febae2a5c
olgasparyan <olgasparyan@gmail.com>
2019-04-17 18:22:12 -0400

    apr17.2

--- js/jquery_button.js
+++ js/jquery_button.js
@@ -1,3 +1,17 @@
-$("#formButton").click(function(){
-        $("#form1").toggle();
-    });
+document.querySelector('#b1').addEventListener('click', function() {
+  document.querySelector('#c1').style.height = 'auto';
+  this.style.display = 'none';
+});
+
+document.querySelector('#b2').addEventListener('click', function() {
+  document.querySelector('#c2').style.height = 'auto';
+  this.style.display = 'none';
+});
+
+$(document).ready(function(){
+  $( "div" ).each(function( index ) {
+    if($(this)[0].scrollHeight <= $(this)[0].offsetHeight){
+      $(this).next().hide();
+    }
+  });
+});
